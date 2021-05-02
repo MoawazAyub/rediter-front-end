c0488bde0147cf820471e5f33aeb431a640e6837
Moawaz Ayub <moawazayubmmm@gmail.com>
2021-05-02 15:24:54 +0500

    added react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from 'react'
-import axios from 'axios'
-import './App.css';
-
-
 class App extends React.Component  {
-  state={
-    data: ""
-  }
-
-  componentDidMount() {
-    this.fetchdata()
-  }
-
-  fetchdata() {
-    axios.get('http://localhost:3000/api/reddit_channel').then((res) => {
-      this.setState({data: res})
-    })
-  }
 
   render() {
     return (
-      <div className="App">
-        {JSON.stringify(this.state.data)}
-      </div>
+      <>
+        <h1>hello</h1>
+        <div>dsfsdf</div>
+      </>
     );
   }
 }
