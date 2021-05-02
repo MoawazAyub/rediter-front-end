b9c9d42fb4c6fb8c1b23e7e7a7b82f8ee98a8eb2
Moawaz Ayub <moawazayubmmm@gmail.com>
2021-05-02 14:53:50 +0500

    added axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react'
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component  {
+  state={
+    data: ""
+  }
+
+  componentDidMount() {
+    this.fetchdata()
+  }
+
+  fetchdata() {
+    axios.get('http://localhost:3000/api/reddit_channel').then((res) => {
+      this.setState({data: res})
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {JSON.stringify(this.state.data)}
+      </div>
+    );
+  }
 }
 
 export default App;
